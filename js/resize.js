b50aedd2a2fa63efdb7d153350edd494bfa28933
César Gtz <cesareomargtz@gmail.com>
2016-12-15 10:06:42 -0700

    cambios echos a los querys,  tamños del logo mal

--- js/resize.js
+++ js/resize.js
@@ -1,4 +1,4 @@
 document.addEventListener("scroll", function() {
     scrollHeight = window.pageYOffset;
-    document.getElementsByClassName("brand-image")[0].style.height = scrollHeight >= 200 ? "50px" : "";
+    document.getElementsByClassName("brand-image")[0].style.height = scrollHeight >= 600 ? "10px" : "";
 }, false);
